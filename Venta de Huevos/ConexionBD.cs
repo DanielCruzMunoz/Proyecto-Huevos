1cb1002b868c697e2e59b29815bca22be0b12712
danie <danie@DANNYPC>
2024-08-29 21:03:07 -0400

    Agregar archivos de proyecto.

---
+++ Venta de Huevos/ConexionBD.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Venta_de_Huevos
+{
+    internal class ConexionBD
+    {
+        //---------------------------Cadena de conenxion PC Daniel------------------------------------>
+        string cadena = "server= DANNYPC\\SQLEXPRESS; database= EmpresaHuevos; integrated security= true";
+        public SqlConnection conectarBD = new SqlConnection();
+        public ConexionBD()
+        {
+            conectarBD.ConnectionString = cadena;
+        }
+        //----------------------------Metodo para abrir la conexion----------------------------------->
+        public void abrir()
+        {
+            try
+            {
+                conectarBD.Open();
+                Console.WriteLine("Conexion Abierta" );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error al abrir BD " + ex.Message);
+            }
+        }
+        //----------------------------Metodo para cerrar la conexion--------------------------------->
+        public void cerrar()
+        {
+            conectarBD.Close();
+            Console.WriteLine("Conexion Cerrada");
+        }
+    }
+
+}
+
---
+++ Venta de Huevos/Formularios/Form-clientes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Venta_de_Huevos.Formularios
+{
+    public partial class Form_clientes : Form
+    {
+        public Form_clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //----------------------------Metodo para llamar la cadena de conexion que esta en la clase creada---------------------------------->
+            ConexionBD Conecxion = new ConexionBD();
+            Conecxion.abrir();
+            
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ConexionBD Conecxion = new ConexionBD();
+            Conecxion.cerrar();
+        }
+    }
+}
