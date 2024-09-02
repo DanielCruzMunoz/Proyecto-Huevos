e5e15361b009126baa0f8c2898e027a83425b213
Daniel De Jesus Cruz Muñoz <daniel.cruz19@estudiante.edu.do>
2024-09-02 09:42:35 -0400

    agrege la conexion de alejandro BD

---
+++ Venta de Huevos/Clases/ConexionBDAle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Venta_de_Huevos.Clases
+{
+    internal class ConexionBDAle
+    {
+        //--------------------------------------Cadena de conexion de la base de datos de alejandro----------------------------->
+        string cadena2 = "server= ; database= ; integrated security= true";
+        public SqlConnection conectarBDAle = new SqlConnection();
+        public ConexionBDAle()
+        {
+            conectarBDAle.ConnectionString = cadena2;
+        }
+
+        //-----------------------------------Metodo para abrir la conexion de la base de datos--------------------------------->
+        public void abrir()
+        {
+            try
+            {
+                conectarBDAle.Open();
+                Console.WriteLine("Conexion abierta");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error al conectar a la base de datos" + ex);
+            }
+        }
+
+        public void Cerrar()
+        { 
+            conectarBDAle.Close();
+            Console.WriteLine();
+        }
+            
+
+            
+    }
+}
